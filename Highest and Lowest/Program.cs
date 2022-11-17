79f9f0a822acfef480aaf2069fc5216ea7f5fd60
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2022-11-17 12:06:41 +0800

    first commit

---
+++ Highest and Lowest/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Highest_and_Lowest
+{
+   static class HighsetAndLowest
+    {
+        public static string HighAndLow(string info)
+        {
+            string [] splitData = info.Split(" ");
+            string max = "";
+            string min= "";
+            foreach (var data in splitData)
+            {
+                if (max.Length == 0 && min.Length == 0)
+                {
+                    max = data;
+                    min = data;
+                }
+                else
+                {
+                    foreach (var data2 in splitData)
+                    {
+                        if (Convert.ToInt32(max) < Convert.ToInt32(data2))
+                        {
+                            max = data2;
+                        }
+                        else
+                        {
+                            if (Convert.ToInt32(min) > Convert.ToInt32(data2))
+                            {
+                                min = data2;
+                            }
+                        }
+                    }
+                }
+            }
+            return $"{max} {min}";
+        }
+    }
+}
---
+++ Highest and Lowest/Run.cs
@@ -0,0 +1,13 @@
+﻿using Highest_and_Lowest;
+namespace Kata
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(HighsetAndLowest.HighAndLow("1 2 3 4 5"));
+            Console.WriteLine(HighsetAndLowest.HighAndLow("1 2 -3 4 5"));
+            Console.WriteLine(HighsetAndLowest.HighAndLow("1 9 3 4 -5"));
+        }
+    }
+}
